62006856bd59c6bd4e5f61b0f0912f8b6893613d
Emmanuel Nwokoma <nwokoma@gigabytedevelopersinc.com>
2020-05-18 22:08:43 +0100

    Reconstruct Ad interaction
    Signed-off-by: Emmanuel Nwokoma <nwokoma@gigabytedevelopersinc.com>

--- app/src/main/java/com/example/goldearn/BreakGame.java
+++ app/src/main/java/com/example/goldearn/BreakGame.java
@@ -1,9 +1,11 @@
 package com.example.goldearn;
 
+import com.example.goldearn.utils.misc.Configs;
 import com.google.android.gms.ads.AdListener;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.InterstitialAd;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,30 +17,18 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-//pascal added
-import android.os.Bundle;
-import androidx.appcompat.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuItem;
-import com.google.android.gms.ads.AdRequest;
-import com.google.android.gms.ads.AdView;
-import com.google.android.gms.ads.MobileAds;
-import com.google.android.gms.ads.RequestConfiguration;
-import com.google.android.gms.ads.initialization.InitializationStatus;
-import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
-import java.util.Arrays;
-
 public class BreakGame extends AppCompatActivity {
-    // Remove the below line after defining your own ad unit ID.
-    private static final String TOAST_TEXT = "Test ads are being shown. "
-            + "To show live ads, replace the ad unit ID in res/values/strings.xml with your own ad unit ID.";
 
     private static final int START_LEVEL = 1;
     private int mLevel;
     private Button mNextLevelButton;
     private InterstitialAd mInterstitialAd;
     private TextView mLevelTextView;
-    private AdView adView;
+
+    private static Context appContext = App.getContext();
+
+    // Screen Size boolean Check
+    private static boolean isTelevision;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,7 +36,7 @@ public class BreakGame extends AppCompatActivity {
         setContentView(R.layout.activity_break_game);
 
         // Create the next level button, which tries to show an interstitial when clicked.
-        mNextLevelButton = ((Button) findViewById(R.id.next_level_button));
+        mNextLevelButton = findViewById(R.id.next_level_button);
         mNextLevelButton.setEnabled(false);
         mNextLevelButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -56,92 +46,56 @@ public class BreakGame extends AppCompatActivity {
         });
 
         // Create the text view to show the level number.
-        mLevelTextView = (TextView) findViewById(R.id.level);
+        mLevelTextView = findViewById(R.id.level);
         mLevel = START_LEVEL;
 
         // Create the InterstitialAd and set the adUnitId (defined in values/strings.xml).
-        mInterstitialAd = newInterstitialAd();
-        loadInterstitial();
+        mInterstitialAd = new InterstitialAd(this);
+        initInterstitialAd();
 
-        // Toasts the test ad message on the screen. Remove this after defining your own ad unit ID.
-        Toast.makeText(this, TOAST_TEXT, Toast.LENGTH_LONG).show();
+        isTelevision = Configs.isTelevision(this);
+    }
 
+    public void initInterstitialAd() {
+        if (BuildConfig.DEBUG) {
+            mInterstitialAd.setAdUnitId(getResources().getString(R.string.test_interstitial_ad_unit_id));
+        } else {
+            mInterstitialAd.setAdUnitId(getResources().getString(R.string.interstitial_ad_unit_id));
+        }
+        requestNewInterstitial();
+        runAdEvents();
+    }
 
-        //Pascal Add a new Code
-        // Initialize the Mobile Ads SDK.
-        MobileAds.initialize(this, new OnInitializationCompleteListener() {
+    private void runAdEvents() {
+        mInterstitialAd.setAdListener(new AdListener() {
             @Override
-            public void onInitializationComplete(InitializationStatus initializationStatus) {}
+            public void onAdLoaded() {
+                mNextLevelButton.setEnabled(true);
+                requestNewInterstitial();
+                Toast.makeText(appContext, "Interstitial Ad Loaded", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onAdFailedToLoad(int errorCode) {
+                mNextLevelButton.setEnabled(true);
+                requestNewInterstitial();
+                Toast.makeText(appContext, "Interstitial Failed to Load", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onAdClosed() {
+                // Proceed to the next level.
+                goToNextLevel();
+                Toast.makeText(appContext, "Interstitial Ad Closed", Toast.LENGTH_SHORT).show();
+            }
         });
-
-        // Set your test devices. Check your logcat output for the hashed device ID to
-        // get test ads on a physical device. e.g.
-        // "Use RequestConfiguration.Builder().setTestDeviceIds(Arrays.asList("ABCDEF012345"))
-        // to get test ads on this device."
-        MobileAds.setRequestConfiguration(
-                new RequestConfiguration.Builder()
-                        .build());
-
-        // Gets the ad view defined in layout/ad_fragment.xml with ad unit ID set in
-        // values/strings.xml.
-        adView = findViewById(R.id.ad_view);
-
-        // Create an ad request.
-        AdRequest adRequest = new AdRequest.Builder().build();
-
-        // Start loading the ad in the background.
-        adView.loadAd(adRequest);
-        //pascal added
     }
 
-
-    //pascal added
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        // Inflate the menu; this adds items to the action bar if it is present.
-//        getMenuInflater().inflate(R.menu.my, menu);
-//        return true;
-//    }
-
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        // Handle action bar item clicks here. The action bar will
-//        // automatically handle clicks on the Home/Up button, so long
-//        // as you specify a parent activity in AndroidManifest.xml.
-//        int id = item.getItemId();
-//        if (id == R.id.action_settings) {
-//            return true;
-//        }
-//        return super.onOptionsItemSelected(item);
-//    }
-
-    /** Called when leaving the activity */
-    @Override
-    public void onPause() {
-        if (adView != null) {
-            adView.pause();
-        }
-        super.onPause();
-    }
-
-    /** Called when returning to the activity */
-    @Override
-    public void onResume() {
-        super.onResume();
-        if (adView != null) {
-            adView.resume();
+    private void requestNewInterstitial() {
+        if (null != mInterstitialAd) {
+            mInterstitialAd.loadAd(new AdRequest.Builder().build());
         }
     }
-
-    /** Called before the activity is destroyed */
-    @Override
-    public void onDestroy() {
-        if (adView != null) {
-            adView.destroy();
-        }
-        super.onDestroy();
-    }
-    //pascal added
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -164,29 +118,6 @@ public class BreakGame extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    private InterstitialAd newInterstitialAd() {
-        InterstitialAd interstitialAd = new InterstitialAd(this);
-        interstitialAd.setAdUnitId(getString(R.string.interstitial_ad_unit_id));
-        interstitialAd.setAdListener(new AdListener() {
-            @Override
-            public void onAdLoaded() {
-                mNextLevelButton.setEnabled(true);
-            }
-
-            @Override
-            public void onAdFailedToLoad(int errorCode) {
-                mNextLevelButton.setEnabled(true);
-            }
-
-            @Override
-            public void onAdClosed() {
-                // Proceed to the next level.
-                goToNextLevel();
-            }
-        });
-        return interstitialAd;
-    }
-
     private void showInterstitial() {
         // Show the ad if it's ready. Otherwise toast and reload the ad.
         if (mInterstitialAd != null && mInterstitialAd.isLoaded()) {
@@ -197,27 +128,32 @@ public class BreakGame extends AppCompatActivity {
         }
     }
 
-    private void loadInterstitial() {
-        // Disable the next level button and load the ad.
-        mNextLevelButton.setEnabled(false);
-        AdRequest adRequest = new AdRequest.Builder()
-                .build();
-        mInterstitialAd.loadAd(adRequest);
-    }
-
-//    private void loadInterstitial() {
-//        // Disable the next level button and load the ad.
-//        mNextLevelButton.setEnabled(false);
-//        AdRequest adRequest = new AdRequest.Builder()
-//                .setRequestAgent("android_studio:ad_template").build();
-//        mInterstitialAd.loadAd(adRequest);
-//    }
-
-
     private void goToNextLevel() {
         // Show the next level and reload the ad to prepare for the level after.
         mLevelTextView.setText("Level " + (++mLevel));
-        mInterstitialAd = newInterstitialAd();
-        loadInterstitial();
+        requestNewInterstitial();
     }
+
+    public static boolean isTelevision() {
+        return isTelevision;
+    }
+
+    /** Called when leaving the activity */
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    /** Called when returning to the activity */
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    /** Called before the activity is destroyed */
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+    //pascal added
 }
