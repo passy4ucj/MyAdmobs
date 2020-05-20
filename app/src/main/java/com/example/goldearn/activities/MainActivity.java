ab72f123a96952a4aeb1e1e2d7eabf452ae6dd45
Emmanuel Nwokoma <nwokoma@gigabytedevelopersinc.com>
2020-05-20 10:47:05 +0100

    Appease the lint
    Signed-off-by: Emmanuel Nwokoma <nwokoma@gigabytedevelopersinc.com>

--- app/src/main/java/com/example/goldearn/activities/MainActivity.java
+++ app/src/main/java/com/example/goldearn/activities/MainActivity.java
@@ -13,8 +13,6 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
